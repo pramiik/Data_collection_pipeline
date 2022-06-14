01c11e201f2874df3bdfcee61825d8ed64efb1bb
pramika Chockalingam Balaji <pramikabalaji@gmail.com>
2022-06-14 18:57:27 +0100

    Code has been Modified

--- test_scraper.py
+++ test_scraper.py
@@ -8,25 +8,42 @@ from setuptools import setup
 from webscraper import scraper
 
 from selenium import webdriver
-from selenium.common.exceptions import ElementNotInteractableException
-import unittest
-import os
+from selenium.webdriver import chrome
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# selenium 4
+from selenium import webdriver
+#driver = webdriver.Chrome('/home/user/drivers/chromedriver')
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+
+
+#from selenium.common.exceptions import ElementNotInteractableException
 #import pandas as pd
 #from pandas.testing import assert_frame_equal
 
+import unittest
+import os
 
 
 class scraperTestCase(unittest.TestCase):
-    def setup(self):
+    def setUp(self):
         self.bot= scraper('colleen hoover','https://www.goodreads.com/')
         
 
     def test_bookurl(self):
-        expected = 'https://www.goodreads.com/search?q=colleen+hoover'
+        expected = "https://www.goodreads.com/search?q=colleen+hoover"
         self.bot.click_search_bar()
         self.bot.search()
         self.bot.click_search_button()
-        actual = self.bot.driver.current_url()
+        self.bot.close_login_popup()
+        actual= self.bot.driver.current_url.text
         self.assertEqual(expected, actual)
 
     def test_finding_container(self):
@@ -36,22 +53,41 @@ class scraperTestCase(unittest.TestCase):
         self.assertIsNotNone(self.bot.finding_containers)
 
     def test_list_books_urls(self):
-        expected = list
+        #expected = type(list)
+        self.bot.click_search_bar()
+        self.bot.search()
+        self.bot.click_search_button()
+        self.bot.finding_containers()
+        self.bot.list_urls_other()
+        self.bot.list_books_urls()
         actual = type(self.bot.list_books_urls())
-        self.assertListEqual(expected, actual)
+        self.assertListEqual(actual)
 
     def test_save_json(self):
+        self.bot.click_search_bar()
+        self.bot.search()
+        self.bot.click_search_button()
+        self.bot.finding_containers()
+        self.bot.list_urls_other()
+        self.bot.list_books_urls()
         self.bot.save_injson()
-        self.assertTrue(os.path.exists('books.json'))
+        self.assertTrue(os.path.exists('/home/pramika/Documents/Aicore/data_collection_project/raw_data/data.json'))
 
     def test_download_images(self):
+        self.bot.click_search_bar()
+        self.bot.search()
+        self.bot.click_search_button()
+        self.bot.finding_containers()
+        self.bot.list_urls_other()
+        self.bot.list_books_urls()
+        self.bot.save_injson()
         self.bot.download_images()
-        self.assertTrue(os.path.exists('images'))
-        self.assertTrue(os.path.exists('images/colleen hoover1.jpg'))
-        self.assertTrue(os.path.exists('images/colleen hoover2.jpg'))
-        self.assertTrue(os.path.exists('images/colleen hoover3.jpg'))
-        self.assertTrue(os.path.exists('images/colleen hoover4.jpg'))
-        self.assertTrue(os.path.exists('images/colleen hoover5.jpg'))
+        self.assertTrue(os.path.exists('/home/pramika/Documents/Aicore/data_collection_project/raw_data/images'))
+        self.assertTrue(os.path.exists('/home/pramika/Documents/Aicore/data_collection_project/raw_data/images/Colleen Hoover0.jpg'))
+        self.assertTrue(os.path.exists('/home/pramika/Documents/Aicore/data_collection_project/raw_data/images/Colleen Hoover1.jpg'))
+        self.assertTrue(os.path.exists('/home/pramika/Documents/Aicore/data_collection_project/raw_data/images/Colleen Hoover2.jpg'))
+        self.assertTrue(os.path.exists('/home/pramika/Documents/Aicore/data_collection_project/raw_data/images/Colleen Hoover3.jpg'))
+        self.assertTrue(os.path.exists('/home/pramika/Documents/Aicore/data_collection_project/raw_data/images/Colleen Hoover4.jpg'))
 
 
 unittest.main(argv=[''], verbosity=5, exit=False)
